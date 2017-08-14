21b81e92dc4a82abd51b9cadeae586a055512c28
kikoken <kikoken@gmail.com>
2017-08-14 11:48:32 -0300

    server and html base

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    sourcemap = require('gulp-sorucemaps'),
     rename = require('gulp-rename');
 
 
 gulp.task('styles',()=>{
     gulp
         .src('assets/sass/tictac.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
         .pipe(rename('tictac.min.csss'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/css'));
 })
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     app = express();
 
+app.set('view engine','pug')
+
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.render('index')
 })
 
 app.listen(3000, function () {
