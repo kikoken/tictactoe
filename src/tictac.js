ba985168218c54e48889e498d25f9160fd13dc9f
kikoken <kikoken@gmail.com>
2017-08-18 15:07:59 -0300

    add icon--x action selection

--- src/tictac.js
+++ src/tictac.js
@@ -1,3 +1,28 @@
 $(document).ready(()=>{
-    new Vivus('svg', {duration: 200, file: 'svg/icon-x.svg'});
+    console.info('Loaded boardgame');
+    
+    /////////
+    var activePlayer =  1,
+        icons = ['.icon--x','.icon--y'],
+        gameboard =  []
+
+    //cast
+    var boxes = $('.game-board li')
+
+    boxes
+        .click((e)=>{
+            console.log('Player:', activePlayer)
+            let el = e.currentTarget
+            let isGamingOption = gameboard.length<9 
+            && gameboard[$(el).index()] == undefined 
+            
+            if(isGamingOption){
+                $(el)
+                .addClass('inactive')
+                .find(icons[activePlayer-1])
+                .addClass('active')
+            }
+            
+            activePlayer==1 ? activePlayer++ : activePlayer--
+        })
 })
