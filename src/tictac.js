427effaa3db2cec7b7e8ce91b1da5f10ce6fde84
kikoken <kikoken@gmail.com>
2017-08-18 15:46:10 -0300

    update control game

--- src/tictac.js
+++ src/tictac.js
@@ -9,20 +9,33 @@ $(document).ready(()=>{
     //cast
     var boxes = $('.game-board li')
 
+    ////////////
+    $('.jugador--id h5 span').text(activePlayer)
+    
     boxes
-        .click((e)=>{
-            console.log('Player:', activePlayer)
-            let el = e.currentTarget
-            let isGamingOption = gameboard.length<9 
-                                && gameboard[$(el).index()] == undefined 
-            
-            if(isGamingOption){
-                $(el)
-                    .addClass('inactive')
-                    .find(icons[activePlayer-1])
-                    .addClass('active')
-            }
-            
-            activePlayer==1 ? activePlayer++ : activePlayer--
-        })
+    .click((e)=>{
+        console.log('Player:', activePlayer)
+        let el = e.currentTarget
+        let isGamingOption = gameboard.length < 9 
+                            && gameboard[$(el).index()] == undefined 
+        
+        if(!isGamingOption){
+            _endOfGame()
+            return;
+        }
+
+        $(el)
+            .addClass('inactive')
+            .find(icons[activePlayer-1])
+            .addClass('active')
+        
+        activePlayer==1 ? activePlayer++ : activePlayer--
+        $('.jugador--id h5 span').text(activePlayer)
+
+        _checkWinner()
+    })
+
+    function _checkWinner() {
+        
+    }
 })
